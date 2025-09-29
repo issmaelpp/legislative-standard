4e7a8c24b51803b004468d9858d23e4313e4d965
Gustavo Ismael Ponce <ismaelp@outlook.com.ar>
2025-09-29 13:34:33 -0300

    Agregar soporte para paneles con FilamentPHP y refactoriza el codigo para usar avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasAvatar;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +14,7 @@ use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasAvatar, HasName
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
@@ -24,6 +28,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar_url',
     ];
 
     /**
@@ -59,5 +64,33 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    /**
+     * Determine if the user can access the given Filament panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return match($panel->getId()) {
+            'admin' => $this->hasRole('Super Admin'),
+            //'test' => $this->hasAnyRole(['Super Admin', 'test']),
+            default => false,
+        };
+    }
+
+    /**
+     * Get the user's avatar URL.
+     */
+    public function getFilamentAvatarUrl(): ?string
+    {
+        return $this->avatar_url ?? null;
+    }
+
+    /**
+     * Get the user's display name.
+     */
+    public function getFilamentName(): string
+    {
+        return $this->name;
     }
 }
